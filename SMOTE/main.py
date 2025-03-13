0bba93ce26a3af9535c49c451afc0117914c49cc
victtorpgl <victorplugaru2004@gmail.com>
2025-03-13 10:40:48 +0100

    me fucking around with the data

--- SMOTE/main.py
+++ SMOTE/main.py
@@ -1,10 +1,16 @@
 import pandas as pd
 import scipy
 import numpy as np
-df = pd.read_csv("DataSets/train_data.csv")
-print(len(df))
-data_np = df.to_numpy()
-no_rows = data_np.shape[0]
-#print(no_rows)
 
-#print(df.columns.to_list())
+
+def pandas_data_frame_creation ():
+    train_df = pd.read_csv("DataSets/train_data.csv")
+    test_df = pd.read_csv("DataSets/test_data.csv")
+    return train_df, test_df
+
+def sort_by_mission_id():
+        return train_df.sort_values(by = 'mission_id', ascending=1)
+
+train_df, test_df = pandas_data_frame_creation()
+sorted_train_df = sort_by_mission_id()
+print(sorted_train_df.head(50))
