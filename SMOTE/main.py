4b560b79a72d314a3ad9890b8eeb0700952e7cac
victtorpgl <victorplugaru2004@gmail.com>
2025-03-13 11:17:08 +0100

    some more sorting

--- SMOTE/main.py
+++ SMOTE/main.py
@@ -8,9 +8,18 @@ def pandas_data_frame_creation ():
     test_df = pd.read_csv("DataSets/test_data.csv")
     return train_df, test_df
 
-def sort_by_mission_id():
-        return train_df.sort_values(by = 'mission_id', ascending=1)
+def filter_by_risk(df, risk):
+    event_ids_to_remove = df.groupby('event_id')['risk'].max() < risk
+    valid_event_ids = event_ids_to_remove[event_ids_to_remove == False].index
+    return df[df['event_id'].isin(valid_event_ids)]
+
+def sort_by_mission_id(df):
+        return df.sort_values(by = 'event_id', ascending=1)
 
 train_df, test_df = pandas_data_frame_creation()
-sorted_train_df = sort_by_mission_id()
+
+filtered_train_df = filter_by_risk(train_df, -4.0)
+
+sorted_train_df = sort_by_mission_id(filtered_train_df)
+
 print(sorted_train_df.head(50))
