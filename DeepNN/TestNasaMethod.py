ac45dfedf506b27b66135914ce9290b7d632a609
BjornVerhoef <b.e.g.verhoef@student.tudelft.nl>
2025-03-13 11:36:30 +0100

    Changes

--- DeepNN/TestNasaMethod.py
+++ DeepNN/TestNasaMethod.py
@@ -5,13 +5,21 @@ import pandas as pd
 import numpy as np 
 
 #data import 
-rawData = pd.read_csv("../DataSets/train_data.csv", usecols=[0,3])
+rawData = pd.read_csv("../DataSets/train_data.csv", usecols=[0,1,3])
 rawData = rawData.to_numpy()
 
 #data collection, split up in input and output
 #input vector X, target vector Y 
-X = np.empty(len(rawData)-13153)
-Y = np.empty(len(rawData)-13153) 
+X = []
+t = []
+
+N_event = 0
+N_totalEvents = 13154
+
+
+    
+           
+    
 
 
 
