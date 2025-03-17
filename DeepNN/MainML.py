3989fa50ec963e8caca709c89912ddd0f8a2284d
BjornVerhoef <b.e.g.verhoef@student.tudelft.nl>
2025-03-17 10:48:05 +0100

    push

--- DeepNN/MainML.py
+++ DeepNN/MainML.py
@@ -1,6 +1,7 @@
 import DataReading as DR
 import torch 
 import numpy as np
+import matplotlib.pyplot as plt
 X, t = DR.readData()
 
 #Define Parameters
@@ -19,6 +20,14 @@ loss_fn = torch.nn.MSELoss(reduction="sum")
 learning_rate = 1e-5
 optimiser = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-for t in range(n_epochs):
+for i in range(n_epochs):
     y_pred = model(X)
-    
+
+    loss = loss_fn(y_pred, t)
+
+    #Backpropagation
+    optimiser.zero_grad()
+    loss.backward()
+    optimiser.step()
+
+
