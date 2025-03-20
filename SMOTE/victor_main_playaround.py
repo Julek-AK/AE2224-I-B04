8f984cd83135b9aa13a7c0a667c6f7f367611eb1
victtorpgl <victorplugaru2004@gmail.com>
2025-03-20 10:28:53 +0100

    bug fixes

--- SMOTE/victor_main_playaround.py
+++ SMOTE/victor_main_playaround.py
@@ -1,5 +1,5 @@
 from Data_Manager import Data_Manager
 
 df = Data_Manager(train_file = "DataSets/train_data.csv", test_file="DataSets/test_data.csv")
-caca = df.load_data()
-print(caca.head())
+train_df = df.load_data()
+print(train_df)
