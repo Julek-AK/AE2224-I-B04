d59dd74a39d27fb586bd3ab5b05f72d7bb0e243b
emxile <80122657+emxile@users.noreply.github.com>
2025-03-20 09:02:25 +0100

    Update hmmLearnAlgorithm.py

--- HiddenMarkovModel/hmmLearnAlgorithm.py
+++ HiddenMarkovModel/hmmLearnAlgorithm.py
@@ -4,54 +4,67 @@ from hmmlearn import hmm
 lengths = []
 
 observations = [np.array([0, 1, 1, 0, 0, 0]).reshape(-1, 1),
-                np.array([0, 0, 0, 0, 0, 0]).reshape(-1, 1),
+                np.array([1, 0, 0, 0, 0, 0]).reshape(-1, 1),
                 np.array([0, 0, 1, 1, 1, 0]).reshape(-1, 1),
                 np.array([0, 0, 0, 1, 1, 1]).reshape(-1, 1),
                 np.array([0, 0, 1, 0, 1, 1]).reshape(-1, 1),
-                np.array([1, 1, 1, 1, 1, 1]).reshape(-1, 1)]
+                np.array([1, 0, 0, 1, 1, 1]).reshape(-1, 1),
+                np.array([0, 0, 1, 0, 1, 0]).reshape(-1, 1),
+                np.array([1, 0, 0, 1, 1, 1]).reshape(-1, 1),
+                np.array([0, 0, 1, 0, 1, 1]).reshape(-1, 1)]
+
+test = np.array([0, 0, 0, 0, 0, 0]).reshape(-1, 1)
 
 print(len(observations[0]))
 
 for obs in observations:
     lengths.append(len(obs))
 
-#gets correct format for hmmlearn
+# gets correct format for hmmlearn
 squishedObservations = np.concatenate(observations)
 
 
-def idealPrediction(observations, lengths):
+
+# function finds the best model for observations (matrices)
+# returns a model
+def idealPrediction(obs, lens):
     models = list()
     scores = list()
-    #tests a few random starts to find the global maximum
+    # tests a few random starts to find the global maximum
     # look at results, see if it makes sense or not. Sometimes is stuck in a local maximum and then this needs to be increased
-    for idx in range (100):
-        #iteration number can also be increased for a lower likelyhood of being stuck
-        model = hmm.CategoricalHMM(n_components = 2, random_state=idx, n_iter=40)
-        model.fit(observations, lengths)
+    for idx in range (150):
+        # iteration number can also be increased for a lower likelyhood of being stuck
+        model = hmm.CategoricalHMM(n_components = 2, random_state=idx, n_iter=30)
+        model.fit(obs, lens)
         models.append(model)
-        scores.append(model.score(observations, lengths))
+        # score shows how good the model is
+        scores.append(model.score(obs, lens))
+        # see if it's working
         print(f'Converged: {model.monitor_.converged}\t\t'
                 f'Score: {scores[-1]}')
         print(f'The best model had a score of {max(scores)}')
+    # returns the best performing model
     return models[np.argmax(scores)]
 
 model1 = idealPrediction(squishedObservations, lengths)
 
-
-currentObservation = observations[0]
-
-states = model1.predict(currentObservation)
+states = model1.predict(test)
 
 print(states)
 print(f'after: {model1.transmat_}')
 
+
+# function for predicting the next observation
+#HAS TO BE REDONE
+# https://github.com/hmmlearn/hmmlearn/issues/171
 def predictNext(model, observations):
     transMatrix = model.transmat_
     prediction = model.predict(observations)
 
     return np.argmax(transMatrix[prediction[-1]])
 
-print(predictNext(model1, currentObservation))
+#print(f'observation predicting: {observations[1]}')
+print(f'next prediction: {predictNext(model1, test)}')
 
 
 
