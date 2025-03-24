f13a0485c9f23a587c2b5e87b43a8727e3288c5c
victtorpgl <victorplugaru2004@gmail.com>
2025-03-24 09:58:53 +0100

    pre_proccessing function added

--- SMOTE/main.py
+++ SMOTE/main.py
@@ -1,14 +1,12 @@
 from Data_Manager import Data_Manager
 
 def main():
-    df = Data_Manager("DataSets/train_data.csv", "DataSets/test_data.csv")
-    df.load_data()
-    df.clean_data()
-    df.filter_by_risk(-4.0)
-    df.sort_by_event_id_time_to_tca()
-    df.create_event_dict()
-    df.df_to_numpy()
-    print(df)
-
+    dm = Data_Manager('train_data.csv' , 'test_data.csv')
+    train_array, test_array = dm.run_pre_processing(risk_threshold=-4.0)
+    print("Preprocessing complete.")
+    print("Train Array shape:", train_array.shape)
+    print("Test Array shape:", test_array.shape)
+    print("Train Array shape:", train_array.shape)
+    print("Test Array shape:", test_array.shape)
 if __name__ == '__main__':
     main()
