Fixed normalising

DeepNN/NNParameters.py
@@ -2,10 +2,10 @@ import torch
 
 
 n_hidden_neurons = 10
-n_epochs = 2000
-loss_fn = torch.nn.MSELoss(reduction="sum")
+n_epochs = 10000
+loss_fn = torch.nn.L1Loss(reduction="sum")
 
-learning_rate = 1e-9
+learning_rate = 1e-3
 
 #10 hidden layers
 model = torch.nn.Sequential(torch.nn.Linear(4, n_hidden_neurons), 