152b4048b303778b87266a7e073c520086381bab
BjornVerhoef <b.e.g.verhoef@student.tudelft.nl>
2025-03-27 12:29:48 +0100

    padding changes

--- DeepNN/padding.py
+++ DeepNN/padding.py
@@ -1,6 +1,10 @@
 #padding
 import numpy as np
-testarray = [[7, 80,90], [5, 40,50], [3.3, 50, 60], [3.1, 60, 60], [2.2, 70, 60], [1.2,41,90], [0.90, 801, 1234]]
+import DataReading as DR
+
+#testarray = [[7, 80,90], [5, 40,50], [3.3, 50, 60], [3.1, 60, 60], [2.2, 70, 60], [1.2,41,90], [0.90, 801, 1234]]
+testarray = DR.readData3("train")[0]
+print(testarray)
 
 #Defenition
 n_cdms = 18
@@ -17,32 +21,37 @@ eventArray = np.full(18, np.nan)
 
 #Fill every CDM on the proper place
 for CDM in testarray:
-    PlaceInEventArray  = int(round((CDM[0]*3))-1) #x3 as there are 3 cdm per day, than round it to get an the proper int -1 as we start with 0
+    print(CDM)
+    PlaceInEventArray  = int(round((CDM[1]*3))-1) #x3 as there are 3 cdm per day, than round it to get an the proper int -1 as we start with 0
     #Get rid of all cdms not within 5 days prior to TCA
     if PlaceInEventArray > n_cdms -1:
         continue
     #Check if there is no other CDM on the place that we want it, if it is, we move it one place, if there is a cdm as well, we will skip this CDM
     if np.isnan(eventArray[PlaceInEventArray]):
-        eventArray[PlaceInEventArray] = CDM[1]
+        eventArray[PlaceInEventArray] = CDM[2] #Aanpassen dit werkt nog niet
     elif np.isnan(eventArray[PlaceInEventArray+1]):
-        eventArray[PlaceInEventArray+1] =CDM[1]
+        eventArray[PlaceInEventArray+1] = CDM[2]
 
 eventArray = np.flip(eventArray)
 print(eventArray)
 
-#Pad the rest 
+
 indicesNotToPad = np.where(~np.isnan(eventArray))[0]
 
-print(indicesNotToPad)
-
+#Pad i such that the first CDM gets backpropagated and the rest forward propagated
 j=0
+ForwardPad = False
 for i in range(len(eventArray)):
     if np.isnan(eventArray[i]):
         eventArray[i] = eventArray[indicesNotToPad[j]]
-    elif ~np.isnan(eventArray[i]) and j < len(indicesNotToPad)-1:
-        j += 1
-        print(j)
 
+    elif ~np.isnan(eventArray[i]) and j < len(indicesNotToPad):
+        if ForwardPad:
+            j += 1
+        if not ForwardPad:
+            ForwardPad = True
+            
+        
 print(eventArray) 
 
         
