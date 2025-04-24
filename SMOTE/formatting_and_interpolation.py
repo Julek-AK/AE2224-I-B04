235c2df186c0ea2d535ea324f61efcce25e5fd03
BenjaminJ139 <benjamin.janssen@kpnmail.nl>
2025-04-24 09:04:37 +0200

    Update formatting_and_interpolation.py

--- SMOTE/formatting_and_interpolation.py
+++ SMOTE/formatting_and_interpolation.py
@@ -21,7 +21,7 @@ def Interpolate_(eventlist, points,accuracy=500, scaling=1):
         output_TCA = np.linspace(min(TCA),max(TCA), points)
         output_Pc = interpolant(output_TCA)
     
-        results.append(np.array([output_Pc]))
+        results.append(np.ndarray.tolist(output_Pc))
         #print(output_TCA, output_Pc)
 
         '''
@@ -36,7 +36,8 @@ def Interpolate_(eventlist, points,accuracy=500, scaling=1):
         plt.ylabel('Pc')
         plt.show()
         '''
-    return results                                                          
+    return np.array(results)
+                                                              
     
 
 '''
@@ -66,4 +67,4 @@ plt.show()
 if __name__ == '__main__':
     #events1 = {1 : np.random.random((2,10)), 2 : np.array([[0,1,4,9,16,25],[0,1,2,3,4,5]]), 4 : np.array([[0,1,8,27,64,125],[0,1,2,3,4,5]])}
     events2 = {1 : np.array([[0,0],[1,1],[4,2],[9,3],[16,4],[25,5]]), 3 : np.array([[0,0],[1,1],[8,2],[27,3],[64,4],[125,5]])}
-    print(Interpolate_(events2, 100))
+    print(Interpolate_(events2, 3))
